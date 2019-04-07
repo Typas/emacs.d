a7ee6154fa891af48a4e64fda6b711fa150f92cb
Typas <b05209038@g.ntu.edu.tw>
2019-04-07 18:55:32 +0800

    modify the mouse wheel behaviour

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -1,16 +1,16 @@
 (require 'init-frame-hooks)
 
-;; (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
-;; (global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
+(global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
+(global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
 
-;; (autoload 'mwheel-install "mwheel")
+(autoload 'mwheel-install "mwheel")
 
-;; (defun sanityinc/console-frame-setup ()
-;;   (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
-;;   (mwheel-install))
+(defun typas/console-frame-setup ()
+  (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
+  (mwheel-install)
+  (setq mouse-wheel-scroll-amount '(0 ((shift) . 1)))
+  (setq mouse-wheel-progressive-speed nil))
 
-
-
-;; (add-hook 'after-make-console-frame-hooks 'sanityinc/console-frame-setup)
+(add-hook 'after-make-console-frame-hooks 'typas/console-frame-setup)
 
 (provide 'init-xterm)
