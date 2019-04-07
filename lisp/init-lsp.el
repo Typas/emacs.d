01c467c487cb315d100b8c2c703131778dc8db9f
Typas <b05209038@g.ntu.edu.tw>
2019-04-08 00:27:02 +0800

    disable flymake coupled from eglot

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,5 +1,7 @@
 ;; use eglot instead of lsp-mode
 
 (require-package 'eglot)
+;; disable flymake from eglot
+(add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
 
 (provide 'init-lsp)
