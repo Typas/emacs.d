e15205fdf5e424a03ea508c044a4e8daca752ba7
Typas <b05209038@g.ntu.edu.tw>
2020-02-14 19:29:52 +0800

    remove flycheck-rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -5,8 +5,4 @@
              (maybe-require-package 'eglot))
     (add-hook 'rust-mode-hook 'eglot-ensure)))
 
-(when (maybe-require-package 'flycheck-rust)
-  (after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
 (provide 'init-rust)
